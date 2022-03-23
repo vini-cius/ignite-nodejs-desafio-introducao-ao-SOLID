64286b0c36da17a31a8610b50e19b0a37d39d6db
Vinicius de Souza Santos <viniciuskt0@gmail.com>
2022-03-22 23:47:53 -0300

    Desafio Commit

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -3,10 +3,22 @@ import { Request, Response } from "express";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.headers;
+
+      const id = user_id.toString();
+
+      const users = this.listAllUsersUseCase.execute({ user_id: id });
+
+      return response.json(users);
+    } catch (err) {
+      return response.status(400).json({
+        error: err.message
+      });
+    }
   }
 }
 
