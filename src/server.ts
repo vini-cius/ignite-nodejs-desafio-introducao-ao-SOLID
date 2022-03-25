43d82217a2275fd4f6e024c07cbdf0dcd7474478
Vinicius de Souza Santos <viniciuskt0@gmail.com>
2022-03-24 22:45:10 -0300

    Desafio 02 - Documentando com Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,9 @@
 import { app } from ".";
+import swaggerUi from 'swagger-ui-express';
+
+import swaggerDocument from './swagger.json';
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 app.listen(3333, () => console.log("Server is running!"));
